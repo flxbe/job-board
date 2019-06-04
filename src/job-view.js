7485026bae5dbb9b1eed3532cab2173df85d1ae1
Felix Bernhardt <felix.bernhardt@mailbox.org>
2019-06-04 16:24:20 +0200

    extract job-view to templates

--- src/job-view.js
+++ src/job-view.js
@@ -1,3 +1,5 @@
+import { renderDetailedJob } from "./templates.js";
+
 export default class JobView {
   constructor(job, onGoBack) {
     this.job = job;
@@ -7,27 +9,6 @@ export default class JobView {
   }
 
   render() {
-    this.node = document.createElement("div");
-    const job = this.job;
-
-    this.node.innerHTML = `
-      <div>
-        <nav class="nav">
-          <a class="nav-link" href="#">Zurück</a>
-        </nav>
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">${job.name}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">
-              ${job.type} in 
-              ${job.location}
-            </h6>
-            <p class="card-text">${job.description}</p>
-          </div>
-        </div>
-      <div>
-    `;
-
-    this.node.querySelector("a").onclick = this.onGoBack;
+    this.node = renderDetailedJob(this.job, this.onGoBack);
   }
 }
--- src/templates.js
+++ src/templates.js
@@ -1,3 +1,29 @@
+export function renderDetailedJob(job, onGoBack) {
+  const node = document.createElement("div");
+
+  node.innerHTML = `
+      <div>
+        <nav class="nav">
+          <a class="nav-link" href="#">Zurück</a>
+        </nav>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">${job.name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">
+              ${job.type} in 
+              ${job.location}
+            </h6>
+            <p class="card-text">${job.description}</p>
+          </div>
+        </div>
+      <div>
+    `;
+
+  node.querySelector("a").onclick = onGoBack;
+
+  return node;
+}
+
 export function renderJob(job, onSelect) {
   const node = document.createElement("div");
   node.classList += "card mb-3";
