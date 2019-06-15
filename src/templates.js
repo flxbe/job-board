4fdf0c4a5c992ae8e2aa7d55d5ed03faff008cdd
Philipp Bischoff <philippbischoff@philipps-mbp.localdomain>
2019-06-16 00:51:08 +0200

    redesigned job template

--- src/templates.js
+++ src/templates.js
@@ -28,17 +28,18 @@ export function renderDetailedJob(job, onGoBack) {
 
 export function renderJob(job, onSelect) {
   const node = document.createElement("div");
-  node.classList += "shadow-sm card mb-4";
+  node.classList += "jb-shadow rounded jb-mb-4 text-dark";
   node.style.cursor = "pointer";
   node.onclick = onSelect;
 
   node.innerHTML = `
-    <div class="card-body">
-      <h5 class="card-title">${job.title}</h5>
-      <h6 class="card-subtitle mb-2 text-muted">
-        ${job.location}
-      </h6>
-      <p class="card-text">${job.description}</p>
+    <div class="jb-p-3">
+      <h4 class="jb-text-primary font-weight-normal">${job.title}</h4>
+      <h5 class="jb-text-dark jb-mb-2">
+        ${job.company},
+        <span class="jb-ml-1 jb-font-weight-light">${job.location}</span>
+      </h5>
+      <p class="jb-text-dark jb-font-weight-light">${job.description}</p>
     </div>
   `;
 
